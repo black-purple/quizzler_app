a8c1c870a1f367193d845f7c3969930050646c9b
black-purple <82895430+black-purple@users.noreply.github.com>
2021-05-18 00:36:34 +0100

    Finished Project

---
+++ android/app/src/main/kotlin/com/example/quizzler_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizzler_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  bool questionAnswer;
+  String questionText;
+
+  Question(String q, bool a) {
+    questionAnswer = a;
+    questionText = q;
+  }
+}
